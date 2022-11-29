6e9c79c17d72066c354970f0e0b8af346422f254
lectures202x <hwangjoosun@gmail.com>
2022-11-29 11:05:03 +0900

    OSC Rules

--- Assets/Test_Indoors_3A1_Collision/Light_Controller_Test.cs
+++ Assets/Test_Indoors_3A1_Collision/Light_Controller_Test.cs
@@ -13,20 +13,29 @@ public class Light_Controller_Test : MonoBehaviour
     {
         //MyLight = GetComponent<Light>();
         //print("MyLight.spotAngle:" + MyLight.spotAngle);
-        GetComponent<HDAdditionalLightData>().SetSpotAngle(10f); //set
+        GetComponent<HDAdditionalLightData>().SetSpotAngle(10f); // OSC "/hdlight/1/spotangle/" + float
         //print("MyLight.spotAngle:" + MyLight.spotAngle);
        
         //print(GetComponent<HDAdditionalLightData>().aspectRatio);
-        GetComponent<HDAdditionalLightData>().aspectRatio = 5f; //set
+        GetComponent<HDAdditionalLightData>().aspectRatio = 5f; // OSC "/hdlight/1/aspectratio/" + float
         //print(GetComponent<HDAdditionalLightData>().aspectRatio);
 
         print(GetComponent<HDAdditionalLightData>().intensity);
-        GetComponent<HDAdditionalLightData>().intensity = 5000f; //set
+        GetComponent<HDAdditionalLightData>().intensity = 5000f; // OSC "/hdlight/1/intensity/" + float
         print(GetComponent<HDAdditionalLightData>().intensity);
 
         print(GetComponent<HDAdditionalLightData>().luxAtDistance);
-        GetComponent<HDAdditionalLightData>().luxAtDistance = 4f; //set
+        GetComponent<HDAdditionalLightData>().luxAtDistance = 4f; // OSC "/hdlight/1/luxat/" + float
         print(GetComponent<HDAdditionalLightData>().luxAtDistance);
+
+        // OSC "/hdlight/area1/luxat/" + float
+        // OSC "/hdlight/area2/luxat/" + float
+        // OSC "/hdlight/spot1/luxat/" + float
+        // OSC "/hdlight/spot2/luxat/" + float
+        // OSC "/hdlight/spot3/luxat/" + float
+        // OSC "/hdlight/spot4/luxat/" + float
+        // OSC "/hdlight/spot5/luxat/" + float
+        // OSC "/hdlight/spot6/luxat/" + float
     }
 
     // Update is called once per frame
